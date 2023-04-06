16c16745951de6fabba887b4fdc226b6a7c2f1d2
AndriiZlt <117361082+AndriiZlt@users.noreply.github.com>
2023-04-06 16:15:26 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
 
+stopBtnRef.setAttribute('disabled', '');
+
+startBtnRef.addEventListener('click', onStartClick);
+stopBtnRef.addEventListener('click', onStopClick);
+
+let currentInterval = null;
+
+function onStartClick() {
+  currentInterval = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+  startBtnRef.setAttribute('disabled', '');
+  stopBtnRef.removeAttribute('disabled', '');
+}
+
+function onStopClick() {
+  clearInterval(currentInterval);
+  startBtnRef.removeAttribute('disabled', '');
+  stopBtnRef.setAttribute('disabled', '');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
